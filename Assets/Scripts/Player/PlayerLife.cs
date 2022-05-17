de10d3ec0290e8c4b3b1b9fafdbbd78bbfdad20f
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-05-17 16:49:29 +0100

    created animation for player death

--- Assets/Scripts/Player/PlayerLife.cs
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 public class PlayerLife : MonoBehaviour
 {
     public static float lifePoints = 10;
+    public Animator player_anim;
+    public static bool isDead = false;
+
+    public void Awake()
+    {
+        player_anim = GetComponentInChildren<Animator>();
+    }
 
     public void Start()
     {
         Life.UpdateLife(lifePoints);
+    }
+
+    public void Update()
+    {
+        Life.UpdateLife(lifePoints);
+        if (lifePoints <= 0)
+        {
+            isDead = true;
+            player_anim.SetBool("isDead", true);
+        }
     }
 }
