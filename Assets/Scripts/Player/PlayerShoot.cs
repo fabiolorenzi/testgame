0b7b8932e09813848d4731785c3dd46c0469ae0a
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-05-19 15:36:55 +0100

    developed logic to shoot

---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float damage = 5f;
+    public float range = 50f;
+    public float bpm = 60f;
+    private float time = 0f;
+
+    public Camera fpsCam;
+
+    private AudioSource audioController;
+
+    [SerializeField]
+    private AudioClip sound;
+    private Animator player_anim;
+
+    public void Start()
+    {
+        audioController = GetComponentInChildren<AudioSource>();
+        player_anim = GetComponentInChildren<Animator>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            if (Time.time >= time)
+            {
+                Shoot();
+                time += 60 / bpm;
+            }
+        }
+    }
+
+    public void Shoot()
+    {
+        RaycastHit hit;
+
+        // the code below is returning true if we touch something with the ray we create
+        player_anim.SetTrigger("Shoot");
+        audioController.clip = sound;
+        audioController.Play();
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
